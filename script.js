0ece03af1a4a2aa25a092fbfef80418a625eed6a
SANJAY k p <80315065+sanjaysanju123@users.noreply.github.com>
2024-12-03 14:05:52 +0530

    added assets and  updated files
    added the hamburger nav  and profile pics

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
+function toggleMenu()
+{
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open"); 
+=======
 function toggleMenu()
 {
     const menu=document.querySelecton(".menu-links");
     const icon=document.querySelecton(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open"); 
+>>>>>>> 1ce63fceff3b204619dbb09ede0c7af3fb19419c
 }
